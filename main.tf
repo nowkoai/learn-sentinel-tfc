31e4f80002cf6d37802f0ee35c8a684a024453df
nowkoai <50360258+nowkoai@users.noreply.github.com>
2022-03-15 02:03:36 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,5 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region = "ap-northeast-1"
+  region = var.region
 }
 
 data "aws_ami" "ubuntu" {
